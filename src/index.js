9c9edb8283a7417140f227b1877e4d1a9d79018b
AssanSav <assane.savadogo81@gmail.com>
2020-09-04 16:06:57 -0400

    Redux global store, thunk middleware and redux-logger added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from "redux"
+import {Provider} from "react-redux"
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+import {rootReducer} from "./reducers"
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let globalStore = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk, logger))
+)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={globalStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

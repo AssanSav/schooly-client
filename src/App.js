8ea7be0cab32f2a197d580b10ab08f8d9e46bee6
AssanSav <assane.savadogo81@gmail.com>
2020-09-05 11:33:06 -0400

    Refact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+import Routes from "./components/Routes";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Routes />
+      </Router>
+    </>
   );
 }
 
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const BASE_URL = "http://localhost:3001"
 
 export const LOGIN_STUDENT = "LOGIN_STUDENT"
+export const FAILED_LOGIN = "FAILED_LOGIN"
